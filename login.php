35bd276ec8af1919d2bc07e2acee24ee09f99c90
Nwokorie Alex V <nwokoriealex20@gmail.com>
2023-06-07 11:25:01 +0100

    feat: updated the form in the login page

--- login.php
+++ login.php
@@ -1,19 +1,19 @@
 <?php
 
+// initializes the session
 session_start();
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-    if ($_POST['username'] == 'backend' && $_POST['password'] == 'group4'){
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if ($_POST['username'] == 'backend' && $_POST['password'] == 'group4') {
 
         // this helps prevent session fixation attacks
         session_regenerate_id(true);
 
         $_SESSION['is_logged_in'] = true;
-        
+
         // redirect to the admin page
         header('Location: admin_page.php');
         exit;
-
     } else {
 
         $error = "Login incorrect";
@@ -25,29 +25,35 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 
 
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="loginStyle.css">
 </head>
-<body>
-    
-    <h2>Login</h2>
 
-    <?php if (!empty($error)): ?>
+<body>
+
+    <h1 id="h1">Airline Booking Form</h1>
+    <h2 align="center">LOGIN</h2>
+
+    <?php if (!empty($error)) : ?>
         <p><?= $error ?></p>
     <?php endif; ?>
 
-    <form method="POST" action="">
-        <label for="username">Username</label>
-        <input type="text" name="username" id="username">
-        <br> <br>
-        <label for="password">Password</label>
-        <input type="password" name="password" id="password">
-        <br> <br>
+    <div class="container">
+        <form method="POST" action="">
+            <label for="username">Username</label>
+            <input type="text" name="username" id="username">
+            <br> <br>
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password">
+            <br> <br>
 
-        <button type="submit">Sign in</button>
-    </form>
+            <center><button type="submit">Sign in</button></center>
+        </form>
+    </div>
 </body>
+
 </html>
