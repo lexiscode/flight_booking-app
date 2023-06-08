e6f0491b622f6a2d5c6fb6b0a6598781f6b698ea
Nwokorie Alex V <nwokoriealex20@gmail.com>
2023-06-08 17:21:56 +0100

    feat: added bs modal in admin_page

--- admin_page.php
+++ admin_page.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="adminStyle.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 
 <body>
@@ -34,14 +35,51 @@
                         
                         <input type="hidden" name="index" value="<?php echo $index; ?>">
                         <button type="submit" id="remove" name="removeTask">Remove</button>
+
+
+
+
+                        <!--Bootstrap-->
+                        <!-- Button trigger modal -->
+                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop<?php echo $index; ?>">
+                        Reschedule
+                        </button>
+
+                        <!-- Modal -->
+                        <div class="modal fade" id="staticBackdrop<?php echo $index; ?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel<?php echo $index; ?>" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="staticBackdropLabel<?php echo $index; ?>">Modal title</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <?php require "the_form.php" ?>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary">Update</button>
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+
+
+
+
+
+
+
+
                         
-                        <a href="reschedule.php" target="_blank" id="reschedule">Reschedule</a>
+                        <!--<a href="reschedule.php" target="_blank" id="reschedule">Reschedule</a>-->
 
                     </div>
                 <?php endforeach; ?><br>
             </div>
         </form>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 
 </html>
