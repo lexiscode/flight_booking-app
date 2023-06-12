5ffc21d9b3204b229137853aa47d3d88aaeb2231
Nwokorie Alex V <nwokoriealex20@gmail.com>
2023-06-12 12:50:15 +0100

    feat: merely just trying out something with js

--- admin_page.php
+++ admin_page.php
@@ -19,25 +19,25 @@
 
             <form action="" method="POST">
                     
-                <button type="submit" id="remove" name="clearLists">Clear All</button>
+                <button type="submit" id="remove" name="clearLists" style="margin-bottom:10px">Clear All</button>
             </form>
                 
                 <?php foreach ($todoList as $index => $airport) : ?>
                     <div class="booking-item">
                         
                         <span class="booking-info"><b>Name:</b> <?php echo $airport['name']; ?></span>
-                        <span class="booking-info"><b>Date:</b> <?php echo $airport['date']; ?></span>
-                        <span class="booking-info"><b>From:</b> <?php echo $airport['from']; ?></span><br>
+                        <span class="booking-info"><b>Email:</b> <?php echo $airport['email']; ?></span>
+                        <span class="booking-info"><b>Phone Number:</b> <?php echo $airport['phoneNo']; ?></span><br>
+                        <span class="booking-info"><b>Adult(s):</b> <?php echo $airport['adults']; ?></span>
+                        <span class="booking-info"><b>Children:</b> <?php echo $airport['children']; ?></span>
+                        <span class="booking-info"><b>Infants:</b> <?php echo $airport['infants']; ?></span><br>
+                        <span class="booking-info"><b>From:</b> <?php echo $airport['from']; ?></span>
                         <span class="booking-info"><b>To:</b> <?php echo $airport['to']; ?></span>
-                        <span class="booking-info"><b>Time:</b> <?php echo $airport['time']; ?></span>
-                        <span class="booking-info"><b>Seat:</b> <?php echo $airport['seat']; ?></span><br>
+                        <span class="booking-info"><b>Time:</b> <?php echo $airport['time']; ?></span><br>
+                        <span class="booking-info"><b>Date:</b> <?php echo $airport['date']; ?></span>
                         <span class="booking-info"><b>Airline:</b> <?php echo $airport['airline']; ?></span>
-                        <span class="booking-info"><b>Adults:</b> <?php echo $airport['adults']; ?></span>
-                        <span class="booking-info"><b>Children:</b> <?php echo $airport['children']; ?></span><br>
-                        <span class="booking-info"><b>Infants:</b> <?php echo $airport['infants']; ?></span>
-                        <span class="booking-info"><b>Fare:</b> <?php echo $airport['fare']; ?></span>
-                        <span class="booking-info"><b>E-mail:</b> <?php echo $airport['email']; ?></span><br>
-                        <span class="booking-info"><b>Phone Number:</b> <?php echo $airport['phoneNo']; ?></span>
+                        <span class="booking-info"><b>Fare:</b> <?php echo $airport['fare']; ?></span><br>
+                        <span class="booking-info"><b>Seat:</b> <?php echo $airport['seat']; ?></span>
                         <span class="booking-info"><b>Message:</b> <?php echo $airport['message']; ?></span><br>
                         
                         <form action="" method="POST">
@@ -85,6 +85,11 @@
         </form>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    
 </body>
 
 </html>
+
+
+
+
--- index.php
+++ index.php
@@ -52,7 +52,7 @@ session_start();
 
 
     </div>
-
+    
 </body>
 
 </html>
